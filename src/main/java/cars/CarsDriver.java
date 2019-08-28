5fd5a72828211aa83a8cfcc5c2d977dab7d6b2ec
Borko Stankovic <borkostankovic83@yahoo.com>
2019-08-27 22:07:31 -0400

    Cars Project

--- src/main/java/cars/CarsDriver.java
+++ src/main/java/cars/CarsDriver.java
@@ -4,14 +4,19 @@ package cars;
 public class CarsDriver {
 
 	public static void main(String[] args) {
+		
 		System.out.println(new Sport().getColor());
-
 		
+		System.out.println(new Luxury().getColor());		
+		System.out.println(new Sport().getType());		
+		System.out.println(new Luxury().getType());
 		
-		System.out.println(new Luxury());
+		Luxury b = new Luxury("BMW");
+		b.letherSeats();
 		
+		Sport c = new Sport("MUSTANG");
+		c.SportSeats(); // 
 		
-
 	}
 
 }
